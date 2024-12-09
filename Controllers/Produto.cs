091c2720915f54164ac73b21253aa173373e9324
Lucas <lucasfargon2008@gmail.com>
2024-12-09 15:00:09 -0300

    feat: Inicio

---
+++ Controllers/Produto.cs
@@ -0,0 +1,30 @@
+﻿namespace ECommerce.Controllers
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+
+        public double Peso { get; set; }
+
+        public string Marca { get; set; }
+
+
+        public Produto()
+        {
+            
+        }
+
+        public Produto(int id, string nome, double preco, double peso, string marca)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Peso = peso;
+            Marca = marca;
+        }
+    }
+}
